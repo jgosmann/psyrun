040ede746ba91a9490f50e217bc2fe14283669c8
Jan Gosmann <jan@hyper-world.de>
2015-02-13 22:45:26 -0500

    Implement parameter space construction.

---
+++ psyrun/pspace.py
@@ -0,0 +1,68 @@
+import itertools
+
+import pandas as pd
+
+
+class _PSpaceObj(object):
+    def __init__(self, keys):
+        self._keys = keys
+
+    def build(self):
+        return pd.DataFrame(self.iterate())
+
+    def iterate(self):
+        raise NotImplementedError()
+
+    def keys(self):
+        return self._keys
+
+    def __add__(self, other):
+        return Sum(self, other)
+
+    def __mul__(self, other):
+        return Product(self, other)
+
+
+class Param(_PSpaceObj):
+    def __init__(self, **params):
+        super(Param, self).__init__(params.keys())
+        self._params = pd.DataFrame(params)
+
+    def iterate(self):
+        for _, row in self._params.iterrows():
+            yield row
+
+
+class Product(_PSpaceObj):
+    def __init__(self, left, right):
+        shared_keys = set(left.keys()).intersection(set(right.keys()))
+        if len(shared_keys) > 0:
+            raise AmbiguousOperationError(
+                'Duplicate param keys: {0}'.format(shared_keys))
+        super(Product, self).__init__(left.keys() + right.keys())
+        self.left = left
+        self.right = right
+
+    def iterate(self):
+        if len(self.left.keys()) == 0:
+            return self.right.iterate()
+        elif len(self.right.keys()) == 0:
+            return self.left.iterate()
+        else:
+            return (pd.concat(item) for item in itertools.product(
+                self.left.iterate(), self.right.iterate()))
+
+
+class Sum(_PSpaceObj):
+    def __init__(self, left, right):
+        super(Sum, self).__init__(set(left.keys() + right.keys()))
+        self.left = left
+        self.right = right
+
+    def iterate(self):
+        return (pd.Series(item, index=self.keys()) for item in itertools.chain(
+            self.left.iterate(), self.right.iterate()))
+
+
+class AmbiguousOperationError(RuntimeError):
+    pass
---
+++ psyrun/tests/test_pspace.py
@@ -0,0 +1,77 @@
+import itertools
+
+import pandas as pd
+import pytest
+
+from psyrun.pspace import AmbiguousOperationError, Param
+
+
+class TestParam(object):
+    def test_empty(self):
+        assert Param().build().empty
+
+    def test_single(self):
+        space = Param(a=[1, 2]).build()
+        assert sorted(space['a']) == [1, 2]
+
+    def test_multiple(self):
+        space = Param(a=[1, 2], b=[3, 4]).build()
+        assert sorted(space['a']) == [1, 2]
+        assert sorted(space['b']) == [3, 4]
+
+    def test_multiple_unequal_length_raises_error(self):
+        with pytest.raises(ValueError):
+            Param(a=[1, 2], b=[3])
+
+    def test_only_scalars_raises_error(self):
+        with pytest.raises(ValueError):
+            Param(a=1, b=2)
+
+    def test_scalars_are_broadcasted(self):
+        space = Param(a=[1, 2], b=3).build()
+        assert sorted(space['a']) == [1, 2]
+        assert sorted(space['b']) == [3, 3]
+
+    def test_knows_about_its_keys(self):
+        assert sorted(Param(a=[0], b=[1]).keys()) == ['a', 'b']
+
+
+class TestProduct(object):
+    def test_product(self):
+        space = (Param(a=[1, 2, 3]) * Param(b=[4, 5])).build()
+        for a, b in itertools.product([1, 2, 3], [4, 5]):
+            assert (space == pd.Series({'a': a, 'b': b})).all(1).any()
+
+    def test_overlapping_product(self):
+        with pytest.raises(AmbiguousOperationError):
+            _ = Param(a=[1, 2]) * Param(a=[2, 3], b=[4, 5])
+
+    def test_product_with_empty(self):
+        space = (Param(a=[1, 2, 3]) * Param()).build()
+        assert sorted(space['a']) == [1, 2, 3]
+
+    def test_product_with_zero_elements(self):
+        space = (Param(a=[1, 2, 3]) * Param(b=[])).build()
+        assert space.empty
+
+
+class TestSum(object):
+    def test_sum(self):
+        space = (Param(a=[1]) + Param(a=[2])).build()
+        assert sorted(space['a']) == [1, 2]
+
+    def test_sum_distinct_params(self):
+        nan_placeholder = 42
+        space = (Param(a=[1]) + Param(b=[2])).build().fillna(nan_placeholder)
+        assert sorted(space['a']) == sorted([1, nan_placeholder])
+        assert sorted(space['b']) == sorted([2, nan_placeholder])
+
+    def test_sum_with_empty(self):
+        space = (Param(a=[1]) + Param()).build()
+        assert sorted(space['a']) == [1]
+
+    def test_sum_with_zero_elements(self):
+        nan_placeholder = 42
+        space = (Param(a=[1]) + Param(b=[])).build().fillna(nan_placeholder)
+        assert sorted(space['a']) == sorted([1])
+        assert sorted(space['b']) == sorted([nan_placeholder])
