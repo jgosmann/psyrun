f6ddc4f8a9fdd3b7ddf36418d58d4bd48f4baa3e
Jan Gosmann <jan@hyper-world.de>
2015-02-16 17:00:29 -0500

    Implement distribute functionality.

--- psyrun/core.py
+++ psyrun/core.py
@@ -1,3 +1,7 @@
+import multiprocessing
+import os
+import os.path
+
 from joblib import Parallel, delayed
 import pandas as pd
 
@@ -9,7 +13,90 @@ def _get_result(fn, row, *args, **kwargs):
     return result
 
 
-def dispatch_threaded(fn, pspace, args, kwargs, n_threads=-1):
+def dispatch_threaded(fn, pspace, args=None, kwargs=None, n_threads=-1):
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = {}
     return pd.concat(Parallel(n_jobs=n_threads)(
         delayed(_get_result)(fn, row, *args, **kwargs)
         for _, row in pspace.iterrows()))
+
+
+def make_local_fn_launcher(fn, n_threads=-1):
+    def launcher(infile, outfile):
+        pspace = load_infile(infile)
+        df = dispatch_threaded(fn, pspace, n_threads=n_threads)
+        save_outfile(df, outfile)
+    return launcher
+
+
+def load_infile(infile):
+    return pd.read_hdf(infile, 'pspace')
+
+
+def save_outfile(df, outfile):
+    return df.to_hdf(outfile, 'results')
+
+
+def load_results(filename):
+    return pd.read_hdf(filename, 'results')
+
+
+def _limited_iter(limit, it):
+    for _ in range(limit):
+        yield next(it)
+
+
+def _get_pspace_dir(base):
+    path = os.path.join(base, 'in')
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return path
+
+
+def _get_results_dir(base):
+    path = os.path.join(base, 'out')
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return path
+
+
+def prepare_distribute(pspace, distribute_dir, n_nodes=None, n_jobs=-1):
+    if n_jobs < 0:
+        try:
+            n_jobs = multiprocessing.cpu_count()
+        except NotImplementedError:
+            n_jobs = 1
+
+    psize = len(pspace)
+    if n_nodes is None:
+        blocksize = n_jobs
+        n_nodes = psize // blocksize
+        if psize % blocksize > 0:
+            n_nodes += 1
+    else:
+        blocksize = (psize + psize % n_nodes) // n_nodes
+
+    it = pspace.iterate()
+    for i in range(n_nodes):
+        block = pd.DataFrame(_limited_iter(blocksize, it))
+        block.to_hdf(os.path.join(
+            _get_pspace_dir(distribute_dir), '{0}.h5'.format(i)), 'pspace')
+
+
+def dispatch_distributed(distribute_dir, launcher):
+    for filename in os.listdir(_get_pspace_dir(distribute_dir)):
+        if os.path.splitext(filename)[1] != '.h5':
+            continue
+        infile = os.path.join(_get_pspace_dir(distribute_dir), filename)
+        outfile = os.path.join(_get_results_dir(distribute_dir), filename)
+        launcher(infile, outfile)
+
+
+def merge_results(distribute_dir, merged_filename):
+    for filename in os.listdir(_get_results_dir(distribute_dir)):
+        if os.path.splitext(filename)[1] != '.h5':
+            continue
+        infile = os.path.join(_get_results_dir(distribute_dir), filename)
+        load_results(infile).to_hdf(merged_filename, 'results', append=True)
--- psyrun/tests/test_core.py
+++ psyrun/tests/test_core.py
@@ -1,8 +1,12 @@
+import os.path
+
 import pandas as pd
 import pytest
 
 from psyrun import Param
-from psyrun.core import dispatch_threaded
+from psyrun.core import (
+    dispatch_threaded, make_local_fn_launcher, load_infile, save_outfile,
+    load_results, prepare_distribute, dispatch_distributed, merge_results)
 
 
 def fn_df(params, arg, foo):
@@ -23,3 +27,19 @@ def test_dispatch_threaded_returning_df(n_threads):
     assert sorted(result['x']) == [1, 1, 4, 8, 9, 27]
     assert sorted(result['arg']) == [23] * 6
     assert sorted(result['foo']) == ['bar'] * 6
+
+
+def square(pspace):
+    return pd.DataFrame({'x': [pspace['a'] ** 2]})
+
+
+def test_distribute(tmpdir):
+    result_path = os.path.join(str(tmpdir), 'result')
+    space = Param(a=range(7))
+    prepare_distribute(space, str(tmpdir), n_jobs=3)
+    dispatch_distributed(
+        str(tmpdir), make_local_fn_launcher(square, n_threads=3))
+    merge_results(str(tmpdir), result_path)
+    result = load_results(result_path)
+    assert sorted(result['a']) == sorted(range(7))
+    assert sorted(result['x']) == [i ** 2 for i in range(7)]
