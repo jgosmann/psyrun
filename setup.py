9daf34f20eb7b45e2e04dc4ce9fb71bb6c8aff02
Jan Gosmann <jan@hyper-world.de>
2015-02-16 12:32:48 -0500

    Setup tox.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='psyrun',
+    version='0.1',
+    author='Jan Gosmann',
+    author_email='jan@hyper-world.de',
+    packages=['psyrun'],
+    provides=['psyrun'],
+)
