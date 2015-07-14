a9c8f4169bb93e5d61f1df90b8ae953e44085f04
Jan Gosmann <jgosmann@uwaterloo.ca>
2015-07-14 14:52:56 -0400

    Test merging of results.

--- psyrun/tests/test_io.py
+++ psyrun/tests/test_io.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from psyrun.io import load_infile, save_infile
+from psyrun.io import append_to_results, load_infile, load_results, save_infile
 from psyrun.pspace import Param
 
 
@@ -13,3 +13,16 @@ def test_infile_with_none(tmpdir):
     for k in pspace:
         for a, b in zip(pspace[k], saved_pspace[k]):
             assert a == b or (np.isnan(a)and np.isnan(b))
+
+
+def test_merging_multidimensional_results(tmpdir):
+    filename = str(tmpdir.join('r.h5'))
+
+    data1 = {'a': np.zeros((2, 2, 2))}
+    data2 = {'a': np.ones((3, 2, 2))}
+
+    append_to_results(data1, filename)
+    append_to_results(data2, filename)
+
+    saved = load_results(filename)
+    assert np.all(np.concatenate([data1['a'], data2['a']]) == saved['a'])
