9e92b69961d83949e29afd85df95313dea20db12
Jan Gosmann <jan@hyper-world.de>
2016-03-27 16:03:26 -0400

    Refactor store.

--- psyrun/store.py
+++ psyrun/store.py
@@ -1,22 +1,77 @@
-"""Function to read and write common psyrun data formats."""
+"""Backends to store load and store data."""
 
 import errno
 
 import numpy as np
 
 
-class DictStore(object):
+class AbstractStore(object):
+    """Base class for classes implementing a store.
+
+    Attributes
+    ----------
+    ext : str
+        Filename extension used by the store.
+    """
+
+    ext = ''
+
     def save(self, filename, data):
+        """Save data to a file.
+
+        Paramaters
+        ----------
+        filename : str
+            Filename of file to save data to.
+        data : dict
+            Dictionary with data to store.
+        """
         raise NotImplementedError()
 
-    def load(self, filename):
+    def load(self, filename, row=None):
+        """Load data from a file.
+
+        Parameters
+        ----------
+        filename : str
+            Filename of file to load data from.
+        row : int, optional
+            If given, only the row with this index will be loaded.
+
+        Returns
+        -------
+        dict
+            Loaded data.
+        """
         raise NotImplementedError()
 
     def append(self, filename, data):
+        """Append data to file.
+
+        When trying to append data to a non-existing file, a new file will be
+        created. The backend may require that a file was created with this
+        function to be able to append to it.
+
+        Parameters
+        ----------
+        filename : str
+            Filename of file to append the data to.
+        data : dict
+            Dictionary with data to append.
+        """
         raise NotImplementedError()
 
 
-class NpzStore(DictStore):
+class NpzStore(AbstractStore):
+    """Store using NumPy `.npz` files.
+
+    This backend needs to load all data in a file to append to it. Similarly,
+    individual rows from a data file can only be returned by loading the whole
+    file. Thus, this backend is not recommended to be used with applications
+    that produce large amounts of data or with load balancing if the parameter
+    space is large.
+    """
+
     ext = '.npz'
 
     def save(self, filename, data):
@@ -58,11 +113,15 @@ class NpzStore(DictStore):
         self.save(filename, loaded)
 
 
-class H5Store(DictStore):
-    """Store and access dictionaries in HDF5 files.
+class H5Store(AbstractStore):
+    """Store using the HDF5 format.
+
+    This store allows for efficient appending as long as the dimensions match.
+
+    Requires pytables to be installed.
 
     Parameters
-    ==========
+    ----------
     node : str, optional
         Node in the HDF5 file to store the data at.
     """
@@ -74,33 +133,12 @@ class H5Store(DictStore):
         self.node = node
 
     def save(self, filename, data):
-        """Save a dictionary in an HDF5 file.
-
-        Parameters
-        ----------
-        filename : str
-            Filename of file to write to.
-        data : dict
-            Data to store.
-        """
         import tables
         with tables.open_file(filename, 'w') as h5:
             for k, v in data.items():
                 h5.create_array(self.node, k, v, createparents=True)
 
     def load(self, filename, row=None):
-        """Read a dictionary from an HDF5 file.
-
-        Parameters
-        ----------
-        filename : str
-            Filename of the file to read from.
-
-        Returns
-        -------
-        dict
-            The data loaded.
-        """
         import tables
         with tables.open_file(filename, 'r') as h5:
             return {node._v_name: node.read(row) for node in h5.iter_nodes(
@@ -108,19 +146,6 @@ class H5Store(DictStore):
 
 
     def append(self, filename, data):
-        """Appends to a saved dictionary in an HDF5 file.
-
-        The shape of already stored data and the new data will be padded with
-        ``nan`` as necessary to make the shapes match. If the stored data has
-        to be padded, this will load all of the stored data into memory.
-
-        Parameters
-        ----------
-        filename : str
-            Filename of file to write to.
-        data : dict
-            Data to store.
-        """
         import tables
         with tables.open_file(filename, 'a') as h5:
             for k, v in data.items():
