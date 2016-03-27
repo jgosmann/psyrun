c59c444712d58b81922f22ebd8739ddbb6b06cb6
Jan Gosmann <jan@hyper-world.de>
2016-03-27 16:41:40 -0400

    Update imports.

--- psyrun/__init__.py
+++ psyrun/__init__.py
@@ -1,2 +1,5 @@
-from .pspace import Param
-from .mapper import map_pspace, map_pspace_parallel
+import psyrun.pspace
+from psyrun.pspace import Param
+from psyrun.mapper import map_pspace, map_pspace_parallel, map_pspace_hdd_backed
+from psyrun.scheduler import ImmediateRun, Sqsub
+from psyrun.store import NpzStore, H5Store
