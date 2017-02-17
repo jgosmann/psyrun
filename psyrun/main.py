14313d8a908b6b9a5df9d376b4585c517a288595
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-02-17 15:20:21 -0500

    Add merge command to main and refactor.

--- psyrun/main.py
+++ psyrun/main.py
@@ -2,6 +2,7 @@ import argparse
 import os.path
 import shutil
 
+from psyrun.processing import Splitter
 from psyrun.psydoit import Fullname, PackageLoader, Submit, Uptodate
 from psyrun.venv import init_virtualenv
 
@@ -29,65 +30,93 @@ def psy_main(argv=None, init_venv=True):
         init_virtualenv()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--taskdir', nargs=1, type=str, default=['psy-tasks'],
-        help="Directory to load tasks from.")
     parser.add_argument('cmd', nargs=1, type=str, help="Command to run.")
     parser.add_argument('args', nargs=argparse.REMAINDER)
     args = parser.parse_args(argv)
 
-    package_loader = PackageLoader(args.taskdir[0])
-    return commands[args.cmd[0]](package_loader, args.args)
+    return commands[args.cmd[0]](args.args).run()
 
 
-def cmd_run(package_loader, argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task', nargs='*', type=str)
-    args = parser.parse_args(argv)
+class Command(object):
+    def __init__(self, argv):
+        self.parser = argparse.ArgumentParser()
+        self.add_args()
+        self.args = self.parser.parse_args(argv)
 
-    for t in package_loader.load_task_defs():
-        if len(args.task) == 0 or t.name in args.task:
-            backend = t.backend(t)
-            job = backend.create_job()
-            names = Fullname(job).names
-            uptodate = Uptodate(job, names, t)
-            Submit(job, names, uptodate)
+    def add_args(self):
+        pass
 
-
-def cmd_clean(package_loader, argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task', nargs='*', type=str)
-    args = parser.parse_args(argv)
-
-    for t in package_loader.load_task_defs():
-        if t.name in args.task:
-            path = os.path.join(t.workdir, t.name)
-            print('rm', path)
-            shutil.rmtree(path)
+    def run(self):
+        raise NotImplementedError()
 
 
-def cmd_list(package_loader, argv):
-    parser = argparse.ArgumentParser()
-    parser.parse_args(argv)
+class TaskdirCmd(Command):
+    def __init__(self, argv):
+        super(TaskdirCmd, self).__init__(argv)
+        self.package_loader = PackageLoader(self.args.taskdir[0])
 
-    for t in package_loader.load_task_defs():
-        print(t.name)
+    def add_args(self):
+        super(TaskdirCmd, self).add_args()
+        self.parser.add_argument(
+            '--taskdir', nargs=1, type=str, default=['psy-tasks'],
+            help="Directory to load tasks from.")
 
 
-def cmd_test(package_loader, argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task', nargs='*', type=str)
-    args = parser.parse_args(argv)
+class TaskselCmd(TaskdirCmd):
+    def add_args(self):
+        super(TaskselCmd, self).add_args()
+        self.parser.add_argument('task', nargs='*', type=str)
 
-    for t in package_loader.load_task_defs():
-        if len(args.task) <= 0 or t.name in args.task:
+
+class RunCmd(TaskselCmd):
+    def run(self):
+        for t in self.package_loader.load_task_defs():
+            if len(self.args.task) == 0 or t.name in self.args.task:
+                backend = t.backend(t)
+                job = backend.create_job()
+                names = Fullname(job).names
+                uptodate = Uptodate(job, names, t)
+                Submit(job, names, uptodate)
+
+
+class CleanCmd(TaskselCmd):
+    def run(self):
+        for t in self.package_loader.load_task_defs():
+            if t.name in self.args.task:
+                path = os.path.join(t.workdir, t.name)
+                print('rm', path)
+                shutil.rmtree(path)
+
+
+class ListCmd(TaskdirCmd):
+    def run(self):
+        for t in self.package_loader.load_task_defs():
             print(t.name)
-            t.execute(**next(t.pspace.iterate()))
+
+
+class MergeCmd(Command):
+    def add_args(self):
+        self.parser.add_argument(
+            'directory', type=str, help="Directory with files to merge.")
+        self.parser.add_argument(
+            'merged', type=str, help="File to write the merged result to.")
+
+    def run(self):
+        Splitter.merge(self.args.directory, self.args.merged)
+
+
+class TestCmd(TaskselCmd):
+    def run(self):
+        for t in self.package_loader.load_task_defs():
+            if len(self.args.task) <= 0 or t.name in self.args.task:
+                print(t.name)
+                t.execute(**next(t.pspace.iterate()))
 
 
 commands.update({
-    'run': cmd_run,
-    'clean': cmd_clean,
-    'list': cmd_list,
-    'test': cmd_test,
+    'run': RunCmd,
+    'clean': CleanCmd,
+    'list': ListCmd,
+    'merge': MergeCmd,
+    'test': TestCmd,
 })
