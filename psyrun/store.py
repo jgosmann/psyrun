5b5839d79f48f115d47be05ff5f201c2a61d8e4b
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-02-20 16:00:06 -0500

    Fix pickle merging.

--- psyrun/store.py
+++ psyrun/store.py
@@ -85,6 +85,14 @@ class PickleStore(AbstractStore):
         else:
             return {k: [v[row]] for k, v in data.items()}
 
+    def _get_data_len(self, data):
+        if len(data) > 0:
+            return max(
+                len(v) if not isinstance(v, string_types + (bytes,)) else 1
+                for v in data.values())
+        else:
+            return 0
+
     def append(self, filename, data):
         try:
             loaded = self.load(filename)
@@ -96,15 +104,23 @@ class PickleStore(AbstractStore):
             keys = set(loaded.keys())
             keys = keys.union(data.keys())
 
+            old_n = self._get_data_len(loaded)
+            new_n = self._get_data_len(data)
+
             for k in keys:
                 if k not in loaded:
-                    n = max(len(v) for v in loaded.values())
-                    loaded[k] = [None] * n
+                    loaded[k] = [None] * old_n
                 if not isinstance(loaded[k], list):
                     loaded[k] = list(loaded[k])
+
                 v = data.get(k, [None])
                 if isinstance(v, string_types + (bytes,)):
                     v = [v]
+                if len(v) != new_n:
+                    if len(v) == 1:
+                        v = v * new_n
+                    else:
+                        raise ValueError("Incompatible data length.")
                 loaded[k].extend(v)
 
             self.save(filename, loaded)
