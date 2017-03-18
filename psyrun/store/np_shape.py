ab07f298630a7f0ff5722a25eade711f2232b896
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-03-18 17:36:04 -0400

    Convert int to float for nans in merge.

--- psyrun/store/np_shape.py
+++ psyrun/store/np_shape.py
@@ -21,12 +21,16 @@ def match_shape(a, shape):
     matched = np.empty((a.shape[0],) + shape, dtype=dtype)
     if np.issubdtype(dtype, float) or np.issubdtype(dtype, complex):
         matched.fill(np.nan)
+    elif np.issubdtype(dtype, int):
+        matched = np.asfarray(matched)
+        matched.fill(np.nan)
     elif dtype.kind in 'SU':
         matched.fill('')
     elif dtype.kind == 'O':
         matched.fill(None)
     else:
-        raise ValueError("Dtype does not support missing values.")
+        raise ValueError(
+            "Dtype {} does not support missing values.".format(dtype))
 
     matched[np.ix_(*(range(x) for x in a.shape))] = a
     return matched
