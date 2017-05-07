148b1ac0bc155d14f33c1e699902bf2877018868
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-05-07 11:02:36 -0400

    Extract code file writing out of submit method.
    Addresses #51.

--- psyrun/backend/base.py
+++ psyrun/backend/base.py
@@ -4,6 +4,66 @@ import os
 import sys
 
 
+class JobSourceFile(object):
+    """Describes a source code file for a job.
+
+    Parameters
+    ----------
+    path : str
+        Path to write the source file to.
+    task : Task
+        Task the source file corresponds to.
+    job_code : str
+        Job specific code to execute.
+
+    Attributes
+    ----------
+    written : bool
+        Whether the source file has been written.
+    full_code : str
+        The full job code including non job-specific parts that are used for
+        every job.
+    """
+
+    def __init__(self, path, task, job_code):
+        self.path = path
+        self.task = task
+        self.job_code = job_code
+        self.written = False
+
+    def write(self):
+        """Write the job code to the file *self.path*."""
+        with open(self.path, 'w') as f:
+            f.write(self.full_code)
+        self.written = True
+
+    @property
+    def full_code(self):
+        return '''
+try:
+    import faulthandler
+    faulthandler.enable()
+except:
+    pass
+
+import time
+print("")
+print("----------------------------------------------------------------------")
+print("Job started ({{}})".format(time.strftime('%a, %d %b %Y %H:%M:%S, %Z')))
+print("----------------------------------------------------------------------")
+
+import os
+os.chdir({taskdir!r})
+
+from psyrun.tasks import TaskDef
+task = TaskDef({taskpath!r})
+{code}
+        '''.format(
+            path=sys.path,
+            taskdir=os.path.abspath(os.path.dirname(self.task.path)),
+            taskpath=os.path.abspath(self.task.path), code=self.job_code)
+
+
 class Backend(object):
     """Abstract base class for processing backends.
 
@@ -49,33 +109,9 @@ class Backend(object):
         dict
             Contains the id of the submitted job under the key ``'id'``.
         """
-        code = '''
-try:
-    import faulthandler
-    faulthandler.enable()
-except:
-    pass
-
-import time
-print("")
-print("----------------------------------------------------------------------")
-print("Job started ({{}})".format(time.strftime('%a, %d %b %Y %H:%M:%S, %Z')))
-print("----------------------------------------------------------------------")
-
-import os
-os.chdir({taskdir!r})
-
-from psyrun.tasks import TaskDef
-task = TaskDef({taskpath!r})
-{code}
-        '''.format(
-            path=sys.path,
-            taskdir=os.path.abspath(os.path.dirname(self.task.path)),
-            taskpath=os.path.abspath(self.task.path), code=code)
         codefile = os.path.join(self.workdir, name + '.py')
         output_filename = os.path.join(self.workdir, name + '.log')
-        with open(codefile, 'w') as f:
-            f.write(code)
+        JobSourceFile(codefile, self.task, code).write()
 
         for job in self.task.scheduler.get_jobs():
             status = self.task.scheduler.get_status(job)
